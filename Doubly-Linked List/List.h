b7dfd7e0a7cfe421bc18c44b291841549782da7d
Jvnc0503 <jvnc0503@gmail.com>
2025-01-19 11:15:30 -0500

    Second commit

---
+++ Doubly-Linked List/List.h
@@ -0,0 +1,69 @@
+#ifndef LIST_H
+#define LIST_H
+
+template<typename T>
+struct Node {
+    T data;
+    Node* next;
+    Node* prev;
+
+    Node() : next(nullptr), prev(nullptr) {
+    }
+
+    explicit Node(T data) : data(data), next(nullptr), prev(nullptr) {
+    }
+
+    Node(Node* next, Node* prev) : next(next), prev(prev) {
+    }
+
+    Node(T data, Node* next, Node* prev) : data(data), next(next), prev(prev) {
+    }
+};
+
+template<typename T>
+class List {
+    Node<T>* head;
+    Node<T>* tail;
+public:
+    List() : head(nullptr), tail(nullptr) {
+    }
+
+    ~List() {
+        auto temp = head;
+        while (temp) {
+            auto next = temp->next;
+            delete temp;
+            temp = next;
+        }
+    }
+
+    T front() {
+        return head ? head->data : T();
+    }
+
+    T back() {
+        return tail ? tail->data : T();
+    }
+
+    void push_front(T data) {
+        auto front = new Node<T>(data, head, nullptr);
+        if (head) {
+            head->prev = front;
+            head = front;
+        } else {
+            head = tail = front;
+        }
+    }
+
+    void push_back(T data) {
+        auto back = new Node<T>(data, nullptr, tail);
+        if (tail) {
+            tail->next = back;
+            tail = back;
+        } else {
+            tail = head = back;
+        }
+    }
+};
+
+#endif
---
+++ Linked List/ForwardList.h
@@ -0,0 +1,166 @@
+#ifndef FORWARDLIST_H
+#define FORWARDLIST_H
+
+template<typename T>
+struct Node {
+    T data;
+    Node *next;
+
+    Node() : next(nullptr) {
+    }
+
+    explicit Node(T data) : data(data), next(nullptr) {
+    }
+
+    explicit Node(Node* next) : next(next) {
+    }
+
+    Node(T data, Node* next) : data(data), next(next) {
+    }
+};
+
+template<typename T>
+class ForwardList {
+    Node<T> *head;
+public:
+    ForwardList() : head(nullptr) {
+    }
+
+    ~ForwardList() {
+        auto temp = head;
+        while (temp) {
+            auto next = temp->next;
+            delete temp;
+            temp = next;
+        }
+    }
+
+    T front() {
+        return head ? head->data : T();
+    }
+
+    T back() {
+        if (head) {
+            auto temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            return temp->data;
+        }
+        return T();
+    }
+
+    void push_front(T data) {
+        auto front = new Node<T>(data, head);
+        head = front;
+    }
+
+    void push_back(T data) {
+        if (head) {
+            auto back = new Node<T>(data);
+            auto temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            temp->next = back;
+        } else {
+            head = new Node<T>(data);
+        }
+    }
+
+    void pop_front() {
+        if (head) {
+            auto temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void pop_back() {
+        if (head) {
+            if (head->next) {
+                auto temp = head;
+                while (temp->next->next) {
+                    temp = temp->next;
+                }
+                delete temp->next;
+                temp->next = nullptr;
+            } else {
+                head = nullptr;
+            }
+        }
+    }
+
+    T operator[](size_t index) {
+        auto temp = head;
+        for (size_t i = 0; i < index; ++i) {
+            temp = temp->next;
+            if (temp == nullptr) {
+                return T();
+            }
+        }
+        return temp->data;
+    }
+
+    bool empty() {
+        return head == nullptr;
+    }
+
+    size_t size() {
+        size_t size = 0;
+        auto temp = head;
+        while (temp) {
+            ++size;
+            temp = temp->next;
+        }
+        return size;
+    }
+
+    void clear() {
+        auto temp = head;
+        while (temp) {
+            auto next = temp->next;
+            delete temp;
+            temp = next;
+        }
+    }
+
+    void sort() {
+        if (head && head->next) {
+            auto current = head;
+            Node<T>* sorted = nullptr;
+            while (current) {
+                auto next = current->next;
+                if (!sorted || sorted->data >= current->data) {
+                    current->next = sorted;
+                    sorted = current;
+                } else {
+                    auto temp = sorted;
+                    while (temp->next && temp->next->data < current->data) {
+                        temp = temp->next;
+                    }
+                    current->next = temp->next;
+                    temp->next = current;
+                }
+                current = next;
+            }
+        }
+    }
+
+    void reverse() {
+        if (head && head->next) {
+            Node<T>* prev = nullptr;
+            auto current = head;
+            auto next = head->next;
+            while (current) {
+                next = current->next;
+                current->next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
+        }
+    }
+};
+
+#endif
