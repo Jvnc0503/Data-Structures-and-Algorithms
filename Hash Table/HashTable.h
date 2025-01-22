7cc46d74eb5246aae456a6f9432acafa4342ac17
Jvnc0503 <jvnc0503@gmail.com>
2025-01-22 15:35:57 -0500

    commit

--- Hash Table/HashTable.h
+++ Hash Table/HashTable.h
@@ -2,37 +2,40 @@
 #define HASHTABLE_H
 
 #include <random>
-#include <cmath>
 
-template <typename T1, typename T2>
+template<typename T1, typename T2>
 struct Node {
     T1 key;
     T2 value;
-    Node* next;
-    explicit Node(T1 key) : key(key), value(T2()), next(nullptr) {}
-    Node(T1 key, T2 value) : key(key), value(value), next(nullptr) {}
+    Node *next;
+
+    explicit Node(T1 key) : key(key), value(T2()), next(nullptr) {
+    }
+
+    Node(T1 key, T2 value) : key(key), value(value), next(nullptr) {
+    }
 };
 
-template <typename T1, typename T2>
+template<typename T1, typename T2>
 class HashTable {
-    Node<T1, T2>** table;
+    Node<T1, T2> **table;
     size_t a, b, size = 100, elements = 0;
     size_t const p = 1e9 + 7;
 
-    size_t hash(const T1& key) {
+    size_t hash(const T1 &key) {
         return ((a * key + b) % p) % size;
     }
 
     size_t hash(const std::string &key) {
         size_t h = 0;
-        for (const char c : key) {
+        for (const char c: key) {
             h = (h * a + c) % p;
         }
         return h % size;
     }
 
     void initialize() {
-        table = new Node<T1, T2>*[size];
+        table = new Node<T1, T2> *[size];
         for (size_t i = 0; i < size; ++i) {
             table[i] = nullptr;
         }
@@ -48,15 +51,15 @@ class HashTable {
     void rehash() {
         const size_t oldSize = size;
         size *= 2;
-        Node<T1, T2>** oldTable = table;
+        Node<T1, T2> **oldTable = table;
         initialize();
         elements = 0;
 
         for (size_t i = 0; i < oldSize; ++i) {
-            Node<T1, T2>* current = oldTable[i];
+            Node<T1, T2> *current = oldTable[i];
             while (current != nullptr) {
                 (*this)[current->key] = current->value;
-                Node<T1, T2>* next = current->next;
+                Node<T1, T2> *next = current->next;
                 delete current;
                 current = next;
             }
@@ -81,9 +84,9 @@ public:
 
     void erase() {
         for (size_t i = 0; i < size; ++i) {
-            Node<T1, T2>* current = table[i];
+            Node<T1, T2> *current = table[i];
             while (current != nullptr) {
-                Node<T1, T2>* next = current->next;
+                Node<T1, T2> *next = current->next;
                 delete current;
                 current = next;
             }
@@ -96,12 +99,12 @@ public:
         return elements == 0;
     }
 
-    T2& operator[](const T1& key) {
+    T2 &operator[](const T1 &key) {
         if (loadFactor() > 0.75) {
             rehash();
         }
         size_t index = hash(key);
-        Node<T1, T2>* current = table[index];
+        Node<T1, T2> *current = table[index];
         if (current == nullptr) {
             table[index] = new Node<T1, T2>(key);
             ++elements;
@@ -118,8 +121,8 @@ public:
         return current->next->value;
     }
 
-    bool isPresent(const T1& key) {
-        Node<T1, T2>* current = table[hash(key)];
+    bool isPresent(const T1 &key) {
+        Node<T1, T2> *current = table[hash(key)];
         while (current != nullptr) {
             if (current->key == key) {
                 return true;
