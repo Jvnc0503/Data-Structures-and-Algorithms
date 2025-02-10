fe65441f596fc89c25af42a5bfcfe867cdeb516d
Jvnc0503 <jvnc0503@gmail.com>
2025-02-10 16:06:25 -0500

    commit

--- AVL Tree/AVL.h
+++ AVL Tree/AVL.h
@@ -7,10 +7,10 @@ template<typename T>
 struct Node {
     T val;
     short balance = 0;
-    Node *left;
-    Node *right;
+    Node *left = nullptr;
+    Node *right = nullptr;
 
-    explicit Node(T val = 0, Node *left = nullptr, Node *right = nullptr): val(val), left(left), right(right) {
+    explicit Node(const T &val): val(val) {
     }
 };
 
@@ -18,38 +18,109 @@ template<typename T>
 class AVL {
     Node<T> *root;
 
-    void LL(Node<T> *&pA) {
-        auto pB = pA->left;
-        pA->left = pB->right;
-        pB->right = pA;
-        pA = pB;
+    Node<T> *LL(Node<T> *A) {
+        Node<T> *B = A->left;
+        A->left = B->right;
+        B->right = A;
+        return B;
     }
 
-    void RR(Node<T> *&pA) {
-        auto pB = pA->right;
-        pA->right = pB->left;
-        pB->left = pA;
-        pA = pB;
+    Node<T> *RR(Node<T> *A) {
+        Node<T> *B = A->right;
+        A->right = B->left;
+        B->left = A;
+        return B;
     }
 
-    void LR(Node<T> *&pA) {
-        auto pB = pA->left;
-        auto pC = pB->right;
-        pB->right = pC->left;
-        pC->left = pB;
-        pA->left = pC->left;
-        pC->right = pA;
-        pA = pC;
+    Node<T> *LR(Node<T> *A) {
+        Node<T> *B = A->left;
+        Node<T> *C = B->right;
+        B->right = C->left;
+        C->left = B;
+        A->left = C->right;
+        C->right = A;
+        return C;
     }
 
-    void RL(Node<T> *&pA) {
-        auto pB = pA->right;
-        auto pC = pB->left;
-        pB->left = pC->right;
-        pC->right = pB;
-        pA->right = pC->left;
-        pC->left = pA;
-        pA = pC;
+    Node<T> *RL(Node<T> *A) {
+        Node<T> *B = A->right;
+        Node<T> *C = B->left;
+        B->left = C->right;
+        C->right = B;
+        A->right = C->left;
+        C->left = A;
+        return C;
+    }
+
+    void insertAux(Node<T> *p, const T &val) {
+        if (val == p->val) {
+            throw std::runtime_error("Duplicate value");
+        }
+        if (val < p->val) {
+            if (p->left == nullptr) {
+                p->left = new Node<T>(val);
+                ++p->balance;
+                return;
+            }
+            if (p->balance == 0) {
+                p->balance = 1;
+            } else if (p->balance == 2) {
+                if (p->left->balance == 1) {
+                    p = LL(p);
+                    p->balance = p->right->balance = 0;
+                } else {
+                    p = LR(p);
+                    if (p->balance == 1) {
+                        p->left->balance = 0;
+                        p->right->balance = -1;
+                    } else if (p->balance == -1) {
+                        p->left->balance = 1;
+                        p->right->balance = 0;
+                    } else {
+                        p->left->balance = p->right->balance = 0;
+                    }
+                    p->balance = 0;
+                }
+            }
+        } else {
+            if (p->right == nullptr) {
+                p->right = new Node<T>(val);
+                --p->balance;
+                return;
+            }
+            if (p->balance == 0) {
+                p->balance = -1;
+            } else if (p->balance == -2) {
+                if (p->right == -1) {
+                    p = RR(p);
+                    p->balance = p->left->balance = 0;
+                } else {
+                    p = RL(p);
+                    if (p->balance == 1) {
+                        p->left->balance = 0;
+                        p->right->balance = -1;
+                    } else if (p->balance == -1) {
+                        p->left->balance = 1;
+                        p->right->balance = 0;
+                    } else {
+                        p->left->balance = p->right->balance = 0;
+                    }
+                    p->balance = 0;
+                }
+            }
+        }
+    }
+
+public:
+    AVL(): root(nullptr) {
+    }
+
+    void insert(const T &val) {
+        if (root == nullptr) {
+            root = new Node(val);
+            return;
+        }
+        insertAux(root, val);
     }
 
     void DFS(Node<T> *p) {
@@ -74,17 +145,6 @@ class AVL {
             if (temp->right != nullptr) {
                 q.push(temp->right);
             }
-        }
-    }
-
-public:
-    AVL(): root(nullptr) {
-    }
-
-    void insert(T val) {
-        if (root == nullptr) {
-            root = new Node(val);
-            return;
         }
     }
 };
