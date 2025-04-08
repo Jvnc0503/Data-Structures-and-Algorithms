46f37144c7e3457f181690de5df026990ee755a5
Jvnc0503 <jvnc0503@gmail.com>
2025-04-07 23:41:40 -0500

    commit

--- BST/BST.h
+++ BST/BST.h
@@ -1,5 +1,6 @@
 #ifndef BST_H
 #define BST_H
+#include <algorithm>
 
 template<typename T>
 struct Node {
@@ -7,7 +8,7 @@ struct Node {
     Node *left;
     Node *right;
 
-    Node(T value, Node *left = nullptr, Node *right = nullptr): val(value), left(left), right(right) {
+    explicit Node(T value, Node *left = nullptr, Node *right = nullptr): val(value), left(left), right(right) {
     }
 
     ~Node() {
@@ -47,11 +48,18 @@ class BST {
         return true;
     }
 
-    Node *&findMin(Node<T> *&node) {
+    Node<T> *&findMin(Node<T> *&node) {
         if (node->left == nullptr) {
             return node;
         }
         return findMin(node->left);
+    }
+
+    Node<T>*& findMax(Node<T> *&node) {
+        if (node->right == nullptr) {
+            return node;
+        }
+        return findMax(node->right);
     }
 
     bool remove(Node<T> *&node, const T &value) {
@@ -60,32 +68,40 @@ class BST {
         }
         if (value < node->val) {
             return remove(node->left, value);
-        } else if (value > node->val) {
-            return remove(node->right, value);
-        } else {
-            if (node->left == nullptr && node->right == nullptr) {
-                delete node;
-                node = nullptr;
-            } else if (node->left == nullptr) {
-                Node<T> *temp = node;
-                node = node->right;
-                temp->right = nullptr;
-                delete temp;
-            } else if (node->right == nullptr) {
-                Node<T> *temp = node;
-                node = node->left;
-                temp->left = nullptr;
-                delete temp;
-            } else {
-                Node<T> *successor = findMin(node->right);
-                node->val = successor->val;
-                Node<T> *temp = successor;
-                successor = successor->right;
-                temp->right = nullptr;
-                delete temp;
-            }
-            return true;
         }
+        if (value > node->val) {
+            return remove(node->right, value);
+        }
+
+        if (node->left == nullptr && node->right == nullptr) {
+            delete node;
+            node = nullptr;
+        } else if (node->left == nullptr) {
+            Node<T> *temp = node;
+            node = node->right;
+            temp->right = nullptr;
+            delete temp;
+        } else if (node->right == nullptr) {
+            Node<T> *temp = node;
+            node = node->left;
+            temp->left = nullptr;
+            delete temp;
+        } else {
+            Node<T> *successor = findMin(node->right);
+            node->val = successor->val;
+            Node<T> *temp = successor;
+            successor = successor->right;
+            temp->right = nullptr;
+            delete temp;
+        }
+        return true;
+    }
+
+    size_t height(Node<T> *node) {
+        if (node == nullptr) {
+            return 0;
+        }
+        return 1 + std::max(height(node->left), height(node->right));
     }
 
 public:
@@ -106,6 +122,18 @@ public:
 
     bool remove(const T &value) {
         return remove(root, value);
+    }
+
+    size_t height() {
+        return height(root);
+    }
+
+    T findMin() {
+        return findMin(root)->val;
+    }
+
+    T findMax() {
+        return findMax(root)->val;
     }
 };
 
