cf2d6ece9ad17955f618d4d5b6d6662af753e65e
Jvnc0503 <jvnc0503@gmail.com>
2025-04-09 19:48:37 -0500

    commit

--- AVL Tree/AVL.h
+++ AVL Tree/AVL.h
@@ -7,27 +7,76 @@
 template<typename T>
 struct Node {
     T val;
-    short height = 0;
-    Node *left = nullptr;
-    Node *right = nullptr;
+    int height;
+    Node *left;
+    Node *right;
 
-    explicit Node(const T &val): val(val) {
-    }
-
-    ~Node() {
-        delete left;
-        delete right;
+    explicit Node(const T &val): val(val), height(0), left(nullptr), right(nullptr) {
     }
 };
 
 template<typename T>
 class AVL {
     Node<T> *root;
-public:
-    AVL() : root(nullptr) {}
 
-    ~AVL() {
-        delete root;
+    void updateHeight(Node<T> *node) const {
+        if (node) {
+            node->height = 1 + std::max(getHeight(node->left), getHeight(node->right));
+        }
+    }
+
+    int getBalance(Node<T> *node) const {
+        return node ? getHeight(node->left) - getHeight(node->right) : 0;
+    }
+
+    Node<T> *LL(Node<T> *A) {
+    }
+
+    Node<T> *RR(Node<T> *A) {
+    }
+
+    Node<T> *LR(Node<T> *A) {
+    }
+
+    Node<T> *RL(Node<T> *A) {
+    }
+
+    Node<T> *balance(Node<T> *node) {
+        const int balance = getBalance(node);
+
+        if (balance == 2) {
+            if (getBalance(node->left) == -1) {
+                return LR(node);
+            }
+            return LL(node);
+        }
+        if (balance == -2) {
+            if (getBalance(node->right) == 1) {
+                return RL(node);
+            }
+            return RR(node);
+        }
+        return node;
+    }
+
+    Node<T> *insert(Node<T> *node, const T &val) {
+        if (node == nullptr) {
+            return new Node<T>(val);
+        }
+        if (val < node->val) {
+            node->left = insert(node->left, val);
+        } else if (val > node->val) {
+            node->right = insert(node->right, val);
+        }
+        updateHeight(node);
+    }
+
+public:
+    AVL() : root(nullptr) {
+    }
+
+    void insert(const T &val) {
+        root = insert(root, val);
     }
 };
 
